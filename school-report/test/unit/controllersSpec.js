4e41b287b875a30a8215f5f0cbaa24c59f9ce033
Romuald Coeffier <rcoeffier@objetdirect.com>
2013-06-11 00:25:52 +0200

    step-07: add a test for controller

--- school-report/test/unit/controllersSpec.js
+++ school-report/test/unit/controllersSpec.js
@@ -3,14 +3,24 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
 
+  var scope;
+  var subjectController;
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  beforeEach(function() {
+    module('myApp.controllers');
+    inject(function($controller, $rootScope, reportService) {
+      scope = $rootScope.$new();
+      subjectController = $controller('SubjectController', {$scope: scope, reportService: reportService});
+    });
+  });
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  it('test', function() {
+    expect(scope.averageOk).toBe(false);
+    scope.newMark = '12';
+    scope.addMarkHandler();
+    scope.$apply();
+    expect(scope.averageOk).toBe(true);
+  });
+
 });
