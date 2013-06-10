6337f014e17539c0023cfcdacfe32212e1b154bd
Romuald Coeffier <rcoeffier@objetdirect.com>
2013-06-11 00:23:37 +0200

    step-04: add controller and binding for subject

--- school-report/app/js/controllers.js
+++ school-report/app/js/controllers.js
@@ -2,10 +2,15 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', ['myApp.services'])
+
+  .controller('MyCtrl1', [function() {}])
+  .controller('MyCtrl2', [function() {}])
+
+  .controller('SubjectController', ['$scope', 'reportService', function ($scope, reportService) {
+
+    $scope.subject = reportService.createSampleSubject();
 
   }])
-  .controller('MyCtrl2', [function() {
 
-  }]);
+;
