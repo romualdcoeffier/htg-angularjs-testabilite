42ddfa716ffbde3c8033ba31510162faee407799
Romuald Coeffier <rcoeffier@objetdirect.com>
2013-06-11 00:20:42 +0200

    step-01: average and report services (with tests)

--- school-report/app/js/services.js
+++ school-report/app/js/services.js
@@ -7,3 +7,71 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1');
+
+
+var averageService = {
+  calculateAverage: function(marks) {
+    var markCount =  marks.length;
+    if (markCount == 0) return null;
+    var average = 0;
+    angular.forEach(marks, function(mark) {average += mark});
+    average = average / markCount;
+    return average;
+  }
+};
+
+
+function Subject(name) {
+  this.name = name;
+  this.marks = [];
+  this.average = null;
+  this.comments = '';
+}
+
+Subject.prototype.addMark = function(mark) {
+  if (! (mark >= 0 && mark <= 20)) return;
+  this.marks.push(mark);
+  this.updateAverage();
+}
+
+Subject.prototype.removeMark= function(index) {
+  this.marks.splice(index, 1);
+  if (this.marks.length > 0) {
+    this.updateAverage();
+  }
+  else {
+    this.average = null;
+  }
+}
+
+Subject.prototype.updateAverage = function() {
+  this.average = averageService.calculateAverage(this.marks);
+}
+
+var reportService = {
+
+  createSubject: function(name) {
+    return new Subject(name);
+  },
+
+  createSampleSubject: function() {
+    var subject = new Subject("Maths");
+    subject.addMark(14);
+    subject.addMark(10);
+    subject.addMark(12);
+    subject.comments = 'Bien';
+    return subject;
+  },
+
+  createSampleReport: function() {
+    var report = [];
+    report.push(this.createSampleSubject());
+    var subject = new Subject("Physique");
+    subject.addMark(5);
+    subject.addMark(8);
+    subject.comments = 'Pas bien';
+    report.push(subject);
+    return report;
+  }
+
+}
--- school-report/test/unit/servicesSpec.js
+++ school-report/test/unit/servicesSpec.js
@@ -3,12 +3,61 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-  beforeEach(module('myApp.services'));
 
+  beforeEach(module('myApp.services'));
 
   describe('version', function() {
     it('should return current version', inject(function(version) {
       expect(version).toEqual('0.1');
     }));
   });
+
+  describe('average', function() {
+
+    it('should return null when no mark', function() {
+      expect(averageService.calculateAverage([])).toEqual(null);
+    });
+
+    it('should return 15 when marks are 15', function() {
+      expect(averageService.calculateAverage([15])).toEqual(15);
+    });
+
+    it('should return 12 when marks are 14, 10, 12', function() {
+      expect(averageService.calculateAverage([14, 10, 12])).toEqual(12);
+    });
+
+  });
+
+  describe('subject', function() {
+
+    it('test subject after creation', function() {
+      var subject = reportService.createSubject('aSubjectName');
+      expect(subject.name).toEqual('aSubjectName');
+      expect(subject.marks).toEqual([]);
+      expect(subject.average).toEqual(null);
+      expect(subject.comments).toEqual('');
+    });
+
+    it('test add/remove marks to subject', function() {
+      var subject = reportService.createSubject('aSubjectName');
+
+      subject.addMark(10);
+      expect(subject.marks).toEqual([10]);
+      expect(subject.average).toEqual(10);
+
+      subject.addMark(12);
+      expect(subject.marks).toEqual([10, 12]);
+      expect(subject.average).toEqual(11);
+
+      subject.removeMark(0);
+      expect(subject.marks).toEqual([12]);
+      expect(subject.average).toEqual(12);
+
+      subject.removeMark(0);
+      expect(subject.marks).toEqual([]);
+      expect(subject.average).toEqual(null);
+    });
+
+  });
+
 });
